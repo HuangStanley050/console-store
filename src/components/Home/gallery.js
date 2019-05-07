24811a27e9a2e505974aed61c5deb3af7c8dabbc
Stanley Huang <huang.stanley050@gmail.com>
2019-05-07 18:08:25 +1000

    skeleton done for gallery component

--- src/components/Home/gallery.js
+++ src/components/Home/gallery.js
@@ -1,8 +1,65 @@
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+const SINGLE_IMAGE = graphql`
+  {
+    img1: file(relativePath: { eq: "homeGallery/xboxone.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    img2: file(relativePath: { eq: "homeGallery/dreamcast.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    img3: file(relativePath: { eq: "homeGallery/playstation.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
 
 const Gallery = props => {
-  return <h1>Gallery</h1>;
+  const wrapperStyle = {
+    padding: "2rem 0",
+    width: "90vw",
+    margin: "0 auto",
+  };
+  return (
+    <StaticQuery
+      query={SINGLE_IMAGE}
+      render={data => {
+        const img1 = data.img1.childImageSharp.fluid;
+        const img2 = data.img2.childImageSharp.fluid;
+        const img3 = data.img3.childImageSharp.fluid;
+        return (
+          <section style={wrapperStyle}>
+            <div className="item item1">
+              <Img fluid={img1} />
+              <p>awesome stuff</p>
+            </div>
+            <div className="item item2">
+              <Img fluid={img2} />
+              <p>awesome stuff</p>
+            </div>
+            <div className="item item3">
+              <Img fluid={img3} />
+              <p>awesome stuff</p>
+            </div>
+          </section>
+        );
+      }}
+    />
+  );
 };
 
 export default Gallery;
